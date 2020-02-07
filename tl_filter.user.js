9832521fe19101cff712b68b74be23844d002f69
DG <dg99@users.noreply.github.com>
2020-02-07 17:07:21 -0600

    Adds .gitignore and initial commit of dummy userscript.

---
+++ tl_filter.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         tl_filter
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Hide TL listings according to various rules
+// @author       DG (dg99@noreply.users.github.com)
+// @match        http://www.torrentleech.org/torrents/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("Hello, world!")
+})();
